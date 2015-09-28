0ad6417e4629b4c9c41faccef96eb3fd67fad760
Gautier Hayoun <ghayoun@gmail.com>
2015-09-28 12:48:48 +0100

    Add nuke_* scripts from mitro-tools

---
+++ tools/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mitro-tools',
+    version='1.0.0',
+    description='Mitro tools',
+    author='Passopolis',
+    license='Private',
+    scripts=['nuke_identity.py', 'nuke_org.py'],
+)
